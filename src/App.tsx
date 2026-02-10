7b064f46a6d6af871e2d4733a0485a660e5d3190
NoahParkes <noahwalterparkes@gmail.com>
2026-02-10 14:42:41 +1300

    ddd basic content
    Added basic content to the page
    
    Refs #2.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,35 @@ import './App.css'
 
 function App() {
   return (
-      <div>
-        Hello World!
-      </div>
+    <div>
+      <h1>
+        Unto the Depths
+      </h1>
+      <RoomEditorWindow />
+    </div>
   )
 }
+function RoomEditorWindow() {
+  return(
+    <div>
+      <RoomCard />
+    </div>
+  )
+}
+
+function RoomCard() {
+  return (
+    <div>
+      <h2>
+        1. Slimy Crossroads
+      </h2>
+      <ol type="A">
+        <li>Room Trap: Well of Filth. Hidden 2</li>
+        <li>Corpse</li>
+        <li>Ladder</li>
+      </ol>
+    </div>
+  )
+}
+
 export default App
