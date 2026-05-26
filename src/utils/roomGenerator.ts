1eb7668a12a81b910af936b84946404f37ae2c08
NoahParkes <noahwalterparkes@gmail.com>
2026-05-26 17:54:14 +1200

    Refactor roomNames 'name' to 'noun'
    Changed roomNames nouns objects name attribute to noun to prevent ambiguity later.
    
    Refs #3

--- src/utils/roomGenerator.ts
+++ src/utils/roomGenerator.ts
@@ -14,7 +14,7 @@ const generateRoomName = () => {
   const capitalise = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
   
   const adjective = capitalise(randomPick(roomNames.adjectives));
-  const noun = capitalise(randomPick(roomNames.nouns).name);
+  const noun = capitalise(randomPick(roomNames.nouns).noun);
 
   if (Math.random() > 0.9) {
     return noun; // 10% chance to just return the noun
